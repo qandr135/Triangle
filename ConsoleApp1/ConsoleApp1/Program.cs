156110163a9c769e2bb348630ebf63e9722dd8c4
Andrey Kim <andrey_kim@epam.com>
2024-01-19 15:24:19 +0500

    console app v1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter character to draw a triangle");
+string character = Console.ReadLine();
+int rows = 7;
+for (int i = 0; i < rows; i++){
+    for (int j = 0; j < rows - i; j++)    {
+        Console.Write(character);
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
